9cc8c11a4aa271c1d8636545b3447ec7ef12fc0a
zuzbrzo <zbrzoska22@gmail.com>
2020-12-02 18:26:16 +0100

    dostosowanie skryptu do użycia z wiersza poleceń

--- scripts/prepare_data_wine.py
+++ scripts/prepare_data_wine.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import argparse
 
 from sklearn.model_selection import train_test_split
 import pickle
@@ -25,14 +26,26 @@ def save_data(x_train, y_train, x_test, y_test, classes_names, output_dir):
 
     print("Pickles saved in ", output_dir)
 
-
+'''
 input_dir = input("Adres danych:")
 #../ZPS2020/datasets/wine
 output_dir = input("Gdzie zapisać:")
 #../ZPS2020/datasets_prepared/wine_new
 test_size_fraction = input('test size fraction:')
 test_size_fraction = float(test_size_fraction)
+'''
+def ParseArguments():
+    parser = argparse.ArgumentParser(description="Project")
+    parser.add_argument('--input-dir', default="", required=True, help='data dir (default: %(default)s)')
+    parser.add_argument('--output-dir', default="", required=True, help='output dir (default: %(default)s)')
+    parser.add_argument('--fraction', default="0.2", required=False,
+                        help='size of test set (fraction) (default: %(default)s)')
+    args = parser.parse_args()
 
+    return args.input_dir, args.output_dir, args.fraction
+
+
+input_dir, output_dir, test_size_fraction = ParseArguments()
 
 test_size_fraction = float(test_size_fraction)
 
